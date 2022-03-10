377bc7a6057bda2454faf45d3c8ee99dadc6cd41
Ankan Poddar <ankan@hyperverge.co>
2022-03-11 00:02:13 +0530

    Create basic documentation using apiDocs

--- server/user/user.routes.js
+++ server/user/user.routes.js
@@ -5,8 +5,62 @@ const { createUserValidationMiddleware, updateUserValidationMiddleware } = requi
 
 const userRouter = express.Router();
 
+/**
+ * @api {get} /api/v1/users Get a list of all users
+ * @apiName GetUserList
+ * @apiGroup User
+ *
+ * @apiSuccess {Object[]} users List of all user objects
+ * @apiSuccess {Number} users.id User Id
+ * @apiSuccess {String} users.name User Name
+ * @apiSuccess {Number} users.age User Age
+ * @apiSuccess {String} [users.email] User Email
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *         "id": 1,
+ *         "name": "Bob Marley",
+ *         "age": 100,
+ *         "email": "marley@harleydavidson.com"
+ *       }
+ *     ]
+ */
 userRouter.get('/', userController.getUsers);
 
+/**
+ * @api {get} /api/v1/users/:id Get details of a user
+ * @apiName GetUserDetails
+ * @apiGroup User
+ *
+ * @apiParam {Number} id User's unique ID
+ *
+ * @apiSuccess {Number} id User Id
+ * @apiSuccess {String} name User Name
+ * @apiSuccess {Number} age User Age
+ * @apiSuccess {String} [email] User Email
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "id": 1,
+ *       "name": "Bob Marley",
+ *       "age": 100,
+ *       "email": "marley@harleydavidson.com"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "status": "failure",
+ *       "statusCode": 404,
+ *       "message": "No user found with id 2"
+ *     }
+}
+ */
 userRouter.get('/:id', userController.getUserById);
 
 userRouter.post(
